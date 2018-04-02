acf99b06ceb9a3d4318e63ffffdda9e2f1cc5894
ukinti <35105009+ukinti@users.noreply.github.com>
2018-04-02 20:10:46 +0500

    Update ne.py

--- ne.py
+++ ne.py
@@ -1,10 +1,6 @@
 import telebot
-tb = telebot.AsyncTeleBot(token='543196227:AAE4nTGtYYlBdsSXdR817_6hjYynyF7yt4Q')
 bot = telebot.TeleBot(token='477042082:AAEBDDi0BizL-RnrRH7tzHyaEDlIjcwL4N8')
-@tb.message_handler(commands=['start'])
-def aio(msg):
-    tb.reply_to(msg, 'async1')
 @bot.message_handler(commands=['help'])
 def aoi(msg):
     bot.reply_to(msg, 'async2')
-((tb)and(bot)).polling(none_stop=True)
+bot.polling(none_stop=True)
